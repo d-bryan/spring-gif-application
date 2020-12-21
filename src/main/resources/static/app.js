3f174772815596cce674e695f6e9338ff7d2f0a4
Dylan Bryan <dylan.g.bryan@gmail.com>
2020-12-21 07:33:29 -0700

    updated javascript functionality

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,3 +2,12 @@ $(function(){
     // Activate mobile nav toggle button
     $(".button-collapse").sideNav({edge: 'right'});
 });
+
+function copyText() {
+    // get the text field
+    const textField = document.getElementById('copy-link').innerHTML;
+    // copy link text to clipboard
+    navigator.clipboard.writeText(textField)
+        .then(() => alert('Text copied to clipboard'))
+        .catch(err => alert('Error in copying text: ' + err.message));
+};
